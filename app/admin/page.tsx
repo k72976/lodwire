469e1def4e00a6ebd8e94be2219a56093bd75105
k72976 <k7297695@gmail.com>
2025-12-11 22:09:33 +0300

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,10 @@
-import { createClient } from "@/lib/supabase/server";
+// import { createClient } from "@/lib/supabase/server";
 
 export default async function Admin(){
-      const supabase = await createClient();
-const { data: users, error } = await supabase.from("users").select();
-console.log("Users:", users);
-console.log("Error:", error);
+//       const supabase = await createClient();
+// const { data: users, error } = await supabase.from("users").select();
+// console.log("Users:", users);
+// console.log("Error:", error);
   return (
     <div>
       /* {error && <p style={{ color: "red" }}>Error: {error.message}</p>}
